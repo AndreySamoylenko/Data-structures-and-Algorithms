951d4b586eaa1a97770a3a59be72cc2353d0f158
Axhel <adya.samsa.228666@gmail.com>
2026-04-23 00:06:36 +1000

    sdfssgf

--- laba3/Hash_tabla.cpp
+++ laba3/Hash_tabla.cpp
@@ -3,6 +3,7 @@
 #include <cstddef>
 
 #include <string>
+#include <vector>
 #include <iostream>
 
 struct Date
@@ -125,23 +126,68 @@ struct PersonalData
     }
 };
 
-template <typename K, typename V>
-class Associate_array
-{
-public:
-    virtual void insert(K k, V v) = 0;
-    virtual void remove(K k, V v) = 0;
-    virtual std::size_t *search(K k) const = 0;
-};
-
 template <typename V>
-class HashTable : public Associate_array<Key, V>
+class HashTable
 {
 private:
     std::pair<Key, V> *table = nullptr;
+    short *status;
+
     std::size_t t_capacity;
     std::size_t t_size = 0;
-    short *status;
+
+    const float shrink_koef = 0.25;
+    const float enlarge_koef = 0.75;
+
+    void shrink_rehash()
+    {
+        std::pair<Key, V> *old_table = table;
+        short *old_status = status;
+        std::size_t t_old_cap = t_capacity;
+
+        t_capacity /= 2;
+        t_size = 0;
+        table = new std::pair<Key, V>[t_capacity];
+        status = new short[t_capacity];
+        for (std::size_t i = 0; i < t_capacity; i++)
+            status[i] = 0;
+
+        for (int i = 0; i < t_old_cap; i++)
+        {
+            if (old_status[i] == 1)
+            {
+                insert(old_table[i].first, old_table[i].second);
+            }
+        }
+
+        delete[] old_status;
+        delete[] old_table;
+    }
+
+    void enlarge_rehash()
+    {
+        std::pair<Key, V> *old_table = table;
+        short *old_status = status;
+        std::size_t t_old_cap = t_capacity;
+
+        t_capacity = (t_capacity > 0 ? t_capacity * 2 : 1); // костыльный вариант решения для t_capacity = 0;
+        t_size = 0;
+        table = new std::pair<Key, V>[t_capacity];
+        status = new short[t_capacity];
+        for (std::size_t i = 0; i < t_capacity; i++)
+            status[i] = 0;
+
+        for (int i = 0; i < t_old_cap; i++)
+        {
+            if (old_status[i] == 1)
+            {
+                insert(old_table[i].first, old_table[i].second);
+            }
+        }
+
+        delete[] old_status;
+        delete[] old_table;
+    }
 
 public:
     HashTable(std::size_t size_)
@@ -175,7 +221,6 @@ public:
     {
         return table[index].second;
     }
-
     Key get_key(std::size_t index)
     {
         return table[index].first;
@@ -184,14 +229,24 @@ public:
     std::size_t hash_(Key key)
     {
         std::size_t index = 0;
+        std::string full_key = "";
 
-        index += key.date.day;
-        index += key.date.month;
-        index += key.date.year;
+        full_key += std::to_string(key.date.day);
+        full_key += std::to_string(key.date.month);
+        full_key += std::to_string(key.date.year);
+        full_key += key.full_name.name + key.full_name.surname + key.full_name.patronymic;
 
-        for (char c : key.full_name.name + key.full_name.surname + key.full_name.patronymic)
+        int counter = 0;
+        int buffer = 0;
+        for (char c : full_key)
         {
-            index += c;
+            buffer += c;
+            counter++;
+            if (counter == 4)
+            {
+                counter = 0;
+                index += buffer;
+            }
         }
 
         return index;
@@ -199,10 +254,10 @@ public:
 
     std::size_t hash(Key key, std::size_t i)
     {
-        return (hash_(key) + i / 2 + i * i / 2) % t_capacity;
+        return (std::size_t)(hash_(key) + (i  + i * i) / 2.0) % t_capacity;
     }
 
-    void insert(Key key, V v) override
+    void insert(Key key, V v)
     {
         if (t_size == t_capacity)
         {
@@ -215,23 +270,31 @@ public:
         do
         {
             std::size_t j = hash(key, i);
-            if (status[j] == 0)
+
+            if (status[j] != 1)
             {
                 table[j].first = key;
                 table[j].second = v;
-                std::cout << "Key " << key << " inserted at index " << j << std::endl;
+                status[j] = 1;
+
+                t_size++;
+                if ((double)(t_size) / (t_capacity) > enlarge_koef)
+                    enlarge_rehash();
                 return;
             }
             i++;
         } while (i != t_capacity);
+
         std::cout << "Key " << key << " not inserted (table is full 2)" << std::endl;
     }
 
-    std::size_t *search(Key key) override
+    std::vector<std::size_t> search(Key key)
     {
+        std::vector<std::size_t> result;
+
         if (t_size == 0)
         {
-            return nullptr; // not found due to emptiness of table
+            return result; // not found due to emptiness of the table
         }
 
         std::size_t i = 0;
@@ -239,21 +302,20 @@ public:
 
         do
         {
-            j = hash(key);
-            if (table[j].first == key)
+            j = hash(key, i);
+            if (table[j].first == key and status[j] == 1)
             {
-                std::cout << "Key " << key << "found at " << j << " index\n";
-                return &j;
+                // std::cout << "Key " << key << " found at " << j << " index\n";
+                result.push_back(j);
             }
 
             i++;
-        } while (i != t_capacity && status[j] != 0);
+        } while (i != t_capacity && status[j] == 2);
 
-        std::cout << "Key " << key << " not found\n";
-        return nullptr; // not found
+        return result; 
     }
 
-    void remove(Key key, V v) override
+    void remove(Key key, V v)
     {
         if (t_size == 0)
         {
@@ -265,19 +327,65 @@ public:
 
         do
         {
-            j = hash(key);
-            if (table[j].first == key && table[j].second == v)
+            j = hash(key, i);
+            if (table[j].first == key && table[j].second == v && status[j] == 1)
             {
                 status[j] = 2;
+                t_size--;
+                if ((double)t_size / t_capacity < shrink_koef)
+                    shrink_rehash();
                 return;
             }
 
             i++;
         } while (i != t_capacity && status[j] != 0);
     }
+
+    void print_table()
+    {
+        for (int i = 0; i < t_capacity; i++)
+        {
+            switch (status[i])
+            {
+            case 0:
+                std::cout << i << " | ---EMPTY--- | ---EMPTY--- \n";
+                break;
+            case 1:
+                std::cout << i << " | " << table[i].first << " | " << table[i].second << '\n';
+                break;
+            case 2:
+                std::cout << i << " | --DELETED-- | --DELETED-- \n";
+                break;
+            default:
+                break;
+            }
+        }
+    }
 };
 
 int main()
 {
+    HashTable<int> ht(10);
+
+    Key k1{{1, 1, 2020}, {"I", "I", "I"}};
+    Key k2{{1, 2, 2020}, {"I", "I", "I"}};
+    Key k3{{1, 3, 2020}, {"I", "I", "I"}};
+    Key k4{{1, 4, 2020}, {"I", "I", "I"}};
+
+    ht.insert(k1, 100);
+    ht.insert(k2, 200);
+    ht.insert(k2, 300);
+    ht.insert(k4, 400);
+    ht.insert(k1, 500);
+    ht.insert(k4, 600);
+    ht.insert(k2, 700);
+    ht.insert(k3, 800);
+    ht.insert(k3, 900);
+
+    ht.print_table();
+
+    // std::cout <<  ht.search(k1);
+    
+
     return 0;
 }
